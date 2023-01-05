c521c3be2baab248e18d767ab5c0241ac698406b
oluwadamilareadeniye@yahoo.com <oluwadamilareadeniye@yahoo.com>
2023-01-06 04:50:28 +0800

    first commit

--- my_project/urls.py
+++ my_project/urls.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from app import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
